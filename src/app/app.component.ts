9536eacc58d1b52f226725d40fb0d89ae418cd41
Fernando Mastropietro <femastro@gmail.com>
2021-11-30 18:28:26 -0300

    Se agrego css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { HtmlTagDefinition } from '@angular/compiler';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
